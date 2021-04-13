e7baab5bacb4dde2fe6938c93379fa5bb73bb397
Alex Dukhno <5074607+alex-dukhno@users.noreply.github.com>
2021-04-13 10:10:06 +0300

    add license complience notice for part of the code from materialize (#49)
    * add license complience notice for part of the code from materialize
    
    * add as much as appropriate notice in Readme

--- src/cursor.rs
+++ src/cursor.rs
@@ -1,4 +1,8 @@
+// Copyright Materialize, Inc. All rights reserved.
 // Copyright 2020 - 2021 Alex Dukhno
+//
+// This file is derived from the materialize project, available at
+// https://github.com/MaterializeInc/materialize.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +15,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#![allow(unused_attributes)]
+#![rustfmt::skip]
 
 use crate::errors::{PayloadError, PayloadErrorKind};
 use std::str;
